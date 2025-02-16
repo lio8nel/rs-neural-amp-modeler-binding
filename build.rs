f25424d4389fe4b5e0eb4f3a6f760882ff737774
Lionel Armanet <lionel.armanet@gmail.com>
2025-02-16 22:52:40 +0100

    refactor: remove commented out code

--- build.rs
+++ build.rs
@@ -22,29 +22,6 @@ fn main() {
         .std("c++17")
         .compile("nam_get_dsp_wrapper");
 
-    // // Compile the C++ wrapper
-    // build
-    //     .header("nam_get_dsp_wrapper.cpp")
-    //     .include("NeuralAmpModelerCore/Dependencies/Eigen")
-    //     .include("NeuralAmpModelerCore/Dependencies/nlohmann")
-    //     .include("NeuralAmpModelerCore/NAM")
-    //     .clang_args(&["-x", "c++", "-std=c++17"]);
-
-    // // Set minimum macOS version if on macOS
-    // if cfg!(target_os = "macos") {
-    //     build.flag("-mmacosx-version-min=11.0");
-    // }
-
-    // build.compile("nam_get_dsp_wrapper");
-
-    // Specify path to the pre-built object files
-    // let object_dir = "NeuralAmpModelerCore/build/tools/CMakeFiles/loadmodel.dir/__/NAM";
-
-    // Link all .o files in the directory
-    // println!("cargo:rustc-link-search=native={}", object_dir);
-    //    println!("cargo:rustc-link-lib=static=NAM");
-    // println!("cargo:rustc-link-lib=static=NeuralAmpModelerCore");
-
     // Link against C++ standard library
     if cfg!(target_os = "macos") {
         println!("cargo:rustc-link-lib=c++");
@@ -57,14 +34,6 @@ fn main() {
         .header("nam_get_dsp_wrapper.h")
         .clang_arg("-xc++")
         .clang_arg("-std=c++17")
-        // .clang_arg(format!("-I{}", "NeuralAmpModelerCore/Dependencies/Eigen"))
-        // .clang_arg(format!(
-        //     "-I{}",
-        //     "NeuralAmpModelerCore/Dependencies/nlohmann"
-        // ))
-        // Enable this if you run into Eigen alignment issues
-        // .clang_arg("-DEIGEN_MAX_ALIGN_BYTES=0")
-        // .clang_arg("-DEIGEN_DONT_VECTORIZE")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
